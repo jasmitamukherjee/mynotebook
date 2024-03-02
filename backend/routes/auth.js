616c17f094fe9443bb902652bda2859e5f04697e
jasmitamukherjee <jasmita.mukherjee.btech2021@sitpune.edu.in>
2024-03-03 01:08:50 +0530

    created login end point

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -62,4 +62,45 @@ res.json({authToken})
 
 })
 
+
+//user authentication
+router.post('/login',[
+    
+    body('email','Enter a valid email address').isEmail(),
+    body('password','Password cannot be blank').exists()
+   
+], async (req,res)=>{
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+     return res.status(400).json({errors : errors.array()})
+    }
+
+    const {email,password} = req.body;
+    try {
+        let user=await User.findOne({email})
+        if(!user){
+            return res.status(400).json({error:"Try logging in with correct credentials"})
+        }
+        const passwordCompare = await bcrypt.compare(password,user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error:"Try logging in with correct credentials"})
+        }
+        const data={
+            user:{
+                id:user.id
+            }
+           }
+        const authToken = jwt.sign(data,JWT_SEC);
+        res.send(authToken)
+        
+    } catch (error) {
+
+        console.error(error.message);
+        res.status(500).send("Internal Server Error: Some issue has occured.")
+        
+    }
+
+})
+
 module.exports= router;
